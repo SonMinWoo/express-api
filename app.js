2da531ed8d2f771144a7d1f66301cafcfde779f5
SonMinWoo <mwson987@\gmail.com>
2020-10-06 14:56:09 +0900

    getUserApi

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const port = 3240;
+
+let users = [
+    {
+        id: 1,
+        name: 'alice'
+    },
+    {
+        id: 2,
+        name: 'bak'
+    },
+    {
+        id: 3,
+        name: 'chris'
+    }
+]
+
+app.get('/', (req, res) => {
+  res.send('Hello World!\n');
+ });
+
+app.get('/users', (req, res) => res.json(users));
+
+app.get('/users/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    if (!id) {
+      return res.status(400).json({error: 'Incorrect id'});
+    }
+  
+    let user = users.filter(user => user.id === id)[0]
+    if (!user) {
+      return res.status(404).json({error: 'Unknown user'});
+    }  
+    return res.json(user);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
